148b6bdaaa7cde3d0bab4775492178ab9a30e821
Jules21 <julesfabien96@gmail.com>
2019-01-25 17:30:10 +0200

    modify product

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,13 @@ class ProductResource extends JsonResource
             'name' =>$this->name,
             'description' =>$this->detail,
             'price' =>$this->price,
-            'stock' =>$this->stock,
+            'stock' =>$this->stock == 0 ? "out of stock ": $this->stock,
             'discount' =>$this->discount,
+            'totalprice' => round( (1 - ($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() ? rand($this->reviews->sum('star')/$this->reviews->count(),2) : 'no rating yet',
+            'link' => [
+                'href' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
